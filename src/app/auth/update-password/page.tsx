715406b175d816cc4d64e173c2b40e50839407ee
hhubert14 <h.hubert@wustl.edu>
2025-06-04 22:46:12 -0500

    feat(auth): add UpdatePasswordForm component and update imports

--- src/app/auth/update-password/page.tsx
+++ src/app/auth/update-password/page.tsx
@@ -1,4 +1,4 @@
-import { UpdatePasswordForm } from "@/components/auth/update-password-form";
+import { UpdatePasswordForm } from "@/app/auth/update-password/UpdatePasswordForm";
 
 export default function Page() {
     return (
