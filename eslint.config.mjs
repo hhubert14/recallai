96763450442d167878f524163d3d95b3ee7bb275
hhubert14 <h.hubert@wustl.edu>
2025-06-27 20:02:59 -0500

    Disable TypeScript ESLint rules for unused variables and explicit any types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,8 +16,9 @@ const eslintConfig = [
         rules: {
             // "indent": ["error", 4],
             // "prettier/prettier": ["error", { "tabWidth": 4 }],
-            "@typescript-eslint/no-unused-vars": "warn",
-            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
             "react/no-unescaped-entities": "off",
         },
     },
