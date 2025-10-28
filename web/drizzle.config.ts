89e3ebb909b97dedde5d04caae1d9d0aec606df4
hhubert14 <h.hubert@wustl.edu>
2025-10-28 08:54:45 -0400

    Add env var check

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -4,12 +4,16 @@ import { defineConfig } from 'drizzle-kit';
 // Explicitly load .env.local
 // dotenv.config({ path: '.env.local' });
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Environment variable DATABASE_URL is required but not set.");
+}
+
 export default defineConfig({
   out: './src/drizzle/migrations',
   schema: './src/drizzle/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
