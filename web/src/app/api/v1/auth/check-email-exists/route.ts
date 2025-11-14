fc50a480ec0e00e0253290a8415c34729dae869e
hhubert14 <h.hubert@wustl.edu>
2025-11-13 20:06:05 -0500

    Use JSend

--- web/src/app/api/v1/auth/check-email-exists/route.ts
+++ web/src/app/api/v1/auth/check-email-exists/route.ts
@@ -1,27 +1,32 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 import { CheckEmailExistsUseCase } from "@/clean-architecture/use-cases/user/check-email-exists.use-case";
 import { createUserRepository } from "@/clean-architecture/infrastructure/factories/repository.factory";
+import { jsendFail, jsendSuccess, jsendError } from "@/lib/jsend";
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
     try {
-        const url = new URL(request.url);
-        const email = url.searchParams.get("email");
+        // const url = new URL(request.url);
+        // const email = url.searchParams.get("email");
+        const { email } = await request.json();
 
         if (!email) {
-            return NextResponse.json(
-                { error: "Email is required" },
-                { status: 400 }
-            );
+            // return NextResponse.json(
+            //     { error: "Email is required" },
+            //     { status: 400 }
+            // );
+            return jsendFail({error: "Email is required"})
         }
         const repo = createUserRepository();
         const emailExists = await new CheckEmailExistsUseCase(repo).execute(email);
 
-        return NextResponse.json({ emailExists });
+        // return NextResponse.json({ emailExists });
+        return jsendSuccess({emailExists})
     } catch (error) {
         console.error("Unexpected error checking email:", error);
-        return NextResponse.json(
-            { error: "Something went wrong" },
-            { status: 500 }
-        );
+        // return NextResponse.json(
+        //     { error: "Something went wrong" },
+        //     { status: 500 }
+        // );
+        return jsendError("Something went wrong")
     }
 }
